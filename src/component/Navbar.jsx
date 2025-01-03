4ca8488b5486629f60b3bd1609e5ea14ce717b07
ritchh12 <riteshsingh3013@gmail.com>
2025-01-03 12:36:23 +0530

    Update source code

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-pink-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg> */}
-      <img className='cursor-pointer' src="public/key.png" alt="" />
+      <img className='cursor-pointer' src="/key.png" alt="" />
       <span className="ml-3 text-red-900 text-xl font-extrabold baloo font-mono cursor-pointer">Pass-Safe</span>
     </a>
     <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
