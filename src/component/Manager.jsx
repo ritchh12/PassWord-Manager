227078390be960341a06b0dd0b74e1e368fd24bf
ritchh12 <riteshsingh3013@gmail.com>
2025-01-03 16:16:48 +0530

    Update source code

--- src/component/Manager.jsx
+++ src/component/Manager.jsx
@@ -124,8 +124,7 @@ const Manager = () => {
 
 
             {/* STARTS  */}
-            <div className="container p-3 m-3 
-flex-wrap: wrap;">
+            <div className="md:container p-3 m-3 flex-wrap wrap;">
                 {/* heading  */}
                 <div className="heading">
                     <h1 className='font-extrabold text-5xl text-center text-pink-700'>&lt; Pass-Safe /&gt;</h1>
@@ -135,18 +134,18 @@ flex-wrap: wrap;">
 
                 {/* website input  */}
                 <div className="website flex justify-center my-5 ">
-                    <input value={form.site} ref={clearRef} onChange={handleChange} type="text" id='' name='site' placeholder='enter website url' className='bg-pink-200 rounded-full w-[70vw] border border-solid border-pink-300 focus:border-white focus:outline-none py-[8px] px-[12px]' />
+                    <input value={form.site} ref={clearRef} onChange={handleChange} type="text" id='' name='site' placeholder='enter website url' className='bg-pink-200 rounded-full w-[100%] md:w-[70vw] border border-solid border-pink-300 focus:border-white focus:outline-none py-[8px] px-[12px]' />
                 </div>
 
                 {/* credentials */}
-                <div className='flex justify-center gap-20' >
+                <div className=' flex flex-col md:flex-row justify-center gap-5 md:gap-20  ' >
 
                     {/* username  */}
-                    <input name='username' value={form.username} onChange={handleChange} type="text" placeholder='enter username' className='bg-pink-200 py-[8px] px-[12px] border border-solid border-pink-300 w-[50vw] rounded-full  focus:border-white focus:outline-none ' />
+                    <input name='username' value={form.username} onChange={handleChange} type="text" placeholder='enter username' className='bg-pink-200 py-[8px] px-[12px] border border-solid border-pink-300 md:w-[50vw] rounded-full  focus:border-white focus:outline-none ' />
 
                     {/* password  */}
-                    <input name='password' onKeyDown={handleKeyDown} ref={ref} value={form.password} onChange={handleChange} type="password" placeholder='enter password' className='bg-pink-200 py-[8px] px-[12px]  border border-solid border-pink-300 w-[15vw] rounded-full  focus:border-white focus:outline-none' />
-                    <span className='absolute right-56 cursor-pointer top-[26 0px]' onClick={showPassword}><lord-icon
+                    <input name='password' onKeyDown={handleKeyDown} ref={ref} value={form.password} onChange={handleChange} type="password" placeholder='enter password' className='bg-pink-200 py-[8px] px-[12px]  border border-solid border-pink-300 md:w-[15vw] rounded-full  focus:border-white focus:outline-none ' />
+                    <span className='absolute right-56 cursor-pointer top-[254px]' onClick={showPassword}><lord-icon
                         src="https://cdn.lordicon.com/dicvhxpz.json"
                         trigger="hover"
                         stroke="bold"
@@ -171,8 +170,8 @@ flex-wrap: wrap;">
                 {/* table  */}
                 <h2 className='font-extrabold text-pink-700 text-2xl text-center'> Your Passwords</h2>
                 {passwordArray.length === 0 && <div className='text-lg text-center m-10  text-pink-600 '>Nothing to Show</div>}
-                {passwordArray.length != 0 && <div className=' p-5  '>
-                    <table className="table-auto w-[90vw] mx-auto rounded-lg overflow-hidden ml-7">
+                {passwordArray.length != 0 && <div className=' md:p-5 overflow-x-auto '>
+                    <table className="table-auto md:w-[90vw] md:mx-auto rounded-lg overflow-hidden md:ml-7">
                         <thead className='bg-pink-700'>
                             <tr className=''>
                                 <th className='px-4 py-2'>Site</th>
@@ -185,7 +184,7 @@ flex-wrap: wrap;">
                             {passwordArray.map((item, index) => {
 
                                 return <tr key={index} className='border border-solid'>
-                                    <td className='px-4 py-2 text-center flex justify-center' >{item.site}<span       onClick={() => handleCopy(item.password)} className="cursor-pointer material-symbols-outlined">
+                                    <td className='px-4 py-2 text-center flex justify-center ' >{item.site}<span       onClick={() => handleCopy(item.password)} className="cursor-pointer material-symbols-outlined">
                                         content_copy
                                     </span></td>
                                     <td className='px-4 py-2 text-center' >{item.username}</td>
@@ -203,7 +202,7 @@ flex-wrap: wrap;">
 
                                     {/* edit */}
                                     </span>
-                                    <span onClick={()=>handleEdit(item.id)} className='cursor-pointer'><img src="/edit.png" width={30}  alt="Edit" />
+                                    <span onClick={()=>handleEdit(item.id)} className='cursor-pointer w-8'><img src="/edit.png"   alt="Edit" />
 </span></td>
                                 </tr>
 
